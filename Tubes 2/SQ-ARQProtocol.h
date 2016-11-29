f7ee4826d48346ad32e8703782878f6edb444106
hafizhmakmur <hafizhmakmur@gmail.com>
2016-11-29 19:26:39 +0700

    Perfecting frame protocol

--- Tubes 2/SQ-ARQProtocol.h
+++ Tubes 2/SQ-ARQProtocol.h
@@ -6,9 +6,9 @@
 #ifndef _SQ_ARQ_Protocol_H_
 #define _SQ_ARQ_Protocol_H_
 
-#define FrameLength 	16		/* Maximum length of message per frame */
+#define MessageLength 	16		/* Maximum length of message per frame */
 
-int messageLength = 5 + FrameLength;
+int frameLength = 5 + MessageLength;
 
 typedef struct FRAME {
 	unsigned int soh;
@@ -25,7 +25,7 @@ typedef struct ACKFormat {
 	Byte checksum;
 } ACKFormat;
 
-Byte getChecksumData(Byte frameno, Byte data[FrameLength]) {
+Byte getChecksumData(Byte frameno, Byte data[MessageLength]) {
 
 	unsigned int sum = 0;
 
@@ -37,13 +37,35 @@ Byte getChecksumData(Byte frameno, Byte data[FrameLength]) {
 
 	int i;
 
-	for (i=0;i<FrameLength;i++) {
+	for (i=0;i<MessageLength;i++) {
 		sum += data[i];
 	}
 
 	sum += ETX;
 
 	return ~sum;
+
+}
+
+Boolean testChecksumData(FRAME frame) {
+
+	unsigned int sum = 0;
+
+	sum += frame.soh;
+
+	sum += frame.frameno;
+
+	sum += frame.stx;
+
+	int i;
+
+	for (i=0;i<MessageLength;i++) {
+		sum += frame.data[i];
+	}
+
+	sum += frame.etx;
+
+	return (sum+frame.checksum) == 0;
 
 }
 
@@ -60,8 +82,20 @@ Byte getChecksumACK(Byte frameno, unsigned int ack) {
 
 }
 
+Boolean testChecksumACK(ACKFormat ackf) {
 
-FRAME getFrame(Byte frameno, Byte data[FrameLength]) {
+	unsigned int sum = 0;
+
+	sum += ackf.frameno;
+	
+	sum += ackf.ack;
+
+	return (sum+ackf.checksum) == 0;
+
+}
+
+
+FRAME createFrame(Byte frameno, Byte data[MessageLength]) {
 	
 	FRAME ret;
 
@@ -81,23 +115,35 @@ FRAME getFrame(Byte frameno, Byte data[FrameLength]) {
 
 }
 
-void createMsg(FRAME frame, Byte message[messageLength]) {
+/*
+void createMsgFrame(FRAME frame, Byte message[frameLength]) {
 
 	message[0] = frame.soh;
 	message[1] = frame.frameno;
 	message[2] = frame.stx;
 
 	int i;
-	for (i=3;i<3+FrameLength;i++) {
+	for (i=3;i<3+MessageLength;i++) {
 		message[i] = frame.data[i-3];
 	}
 
-	message[3+FrameLength] = frame.etx;
-	message[4+FrameLength] = frame.checksum;
+	message[3+frameLength] = frame.etx;
+	message[4+frameLength] = frame.checksum;
 
 }
+*/
 
-ACKFormat getACK(Byte frameno, unsigned int ack) {
+/*
+FRAME getFrame(Byte message[frameLength]) {
+
+	FRAME ret;
+
+	if (message[0] != SOH) return NULL;
+	ret.soh = SOH;
+}
+*/
+
+ACKFormat createACK(Byte frameno, unsigned int ack) {
 	ACKFormat af;
 
 	af.ack = ack;
--- Tubes 2/transmitter.c
+++ Tubes 2/transmitter.c
@@ -10,72 +10,26 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
-#include "dcomm.h" 
+#include "dcomm.h"
 #include "SQ-ARQProtocol.h"
 
 static Byte *recieved;
 Boolean *shtdown;
 
-
+// Error message
 void error(const char *msg) {
     perror(msg);
     exit(0);
 }
 
-int recvACK(int sockfd, char * msg, int length) {
-	do {
-
-		char ch[1];
-		peer_addr_len = sizeof(struct sockaddr_storage);
-		recvfrom(sockfd,ch,1,0,(struct sockaddr *) &peer_addr, &peer_addr_len);
-		*recieved = ch[0];
-
-		if (*recieved == XOFF) {
-			printf("XOFF diterima\n");
-		} else if (*recieved == XON) {
-			printf("XON diterima\n");
-		}
-	
-	} while (!*shtdown);
-
-	return 0;
-}
-
-int sendFrame(int sockfd, char * msg, int length) {
-
-
-	return 0;
-}
-
-
-
-int main(int argc, char *argv[]) {
+// Binding to a socket
+int initConnecton(int argc, char *argv[]) {
 
 	int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char buffer[256], filename[256];
-
-    /* code */
-
-	recieved = mmap(NULL, sizeof *recieved, PROT_READ | PROT_WRITE, 
-                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-
-	*recieved = XON;
-
-
-	shtdown = mmap(NULL, sizeof *shtdown, PROT_READ | PROT_WRITE, 
-                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-
-	*shtdown = false;
-
-    if (argc < 4) {
-       fprintf(stderr,"usage %s hostname port filename\n", argv[0]);
-       exit(0);
-    }
 
     portno = atoi(argv[2]);
-    strcpy(filename,argv[3]);
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
@@ -98,80 +52,182 @@ int main(int argc, char *argv[]) {
 
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    
 
-    struct sockaddr_storage peer_addr;
-    socklen_t peer_addr_len;
+    return sockfd;
+}
 
-	pid_t  pid;
-	pid = fork();
-	
-	if (pid == 0)  {
-		
+//Data link layer for recieving ACK
+ACKFormat recvACK(int sockfd, struct sockaddr_storage peer_addr, socklen_t peer_addr_len) {
 
-		printf("Child finished\n");
+    ACKFormat ack;
+    Byte message[3];
 
-	} else {;
-    	char red;
-    	int i = 0;
+    int i = 0;
+	do {
 
-		while (i < length) {
-			red = msg[i];
+		char ch[1];
+		peer_addr_len = sizeof(struct sockaddr_storage);
+		recvfrom(sockfd,ch,1,0,(struct sockaddr *) &peer_addr, &peer_addr_len);
+		*recieved = ch[0];
 
-			while (*recieved != XON) {
-				printf("Menunggu XON...\n");
-				sleep(1);
-			}
-
-			printf("Mengirim byte ke-%d: %c,'%d'\n",i+1,red,red);
-			
-			char buf[1];
-			buf[0] = red;
-			peer_addr_len = sizeof(struct sockaddr_storage);
-			sendto(sockfd, buf, 1, 0, (struct sockaddr *) &peer_addr, peer_addr_len);
-
+		if (*recieved == XOFF) {
+			printf("XOFF diterima\n");
+		} else if (*recieved == XON) {
+			printf("XON diterima\n");
+		} else {
+			message[i] = *recieved;
 			i++;
-			usleep(25 * 1000);
+		}
+	
+	} while (i<3);
 
+	// Create stream of byte to struct
+	memcpy(&ack,message,sizeof(message));
+
+	return ack;
+}
+
+// Data Link Layer for sending a frame
+void sendFrame(int sockfd, FRAME frame, struct sockaddr_storage peer_addr, socklen_t peer_addr_len) {
+
+	// Variable declaration
+	char red;
+	int i = 0;
+
+	// Create stream of byte from a frame
+	Byte msg[sizeof(frame)];
+	memcpy(msg, &frame, sizeof(frame));
+
+	// Sending message byte per byte
+	while (i < MessageLength) {
+		red = msg[i];
+
+		while (*recieved != XON) {
+			printf("Menunggu XON...\n");
+			sleep(1);
 		}
 
-		*shtdown = true;
+		printf("Mengirim byte ke-%d: %c,'%d'\n",i+1,red,red);
 		
-		printf("Send finished\n");
+		char buf[1];
+		buf[0] = red;
+		peer_addr_len = sizeof(struct sockaddr_storage);
+		sendto(sockfd, buf, 1, 0, (struct sockaddr *) &peer_addr, peer_addr_len);
 
-		wait(NULL);
+		i++;
+		usleep(25 * 1000);
 
 	}
 
-	munmap(recieved, sizeof *recieved);
-	munmap(shtdown, sizeof *shtdown);
-	FILE *f = fopen(filename,"rb");
-
-	int i = 1;
+	*shtdown = true;
 	
-	char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
+	printf("Send finished\n");
 
-	while ((read = getline(&line, &len, f)) != -1) {
+	wait(NULL);
+}
 
-        printf("Mengirim string ke-%d dengan panjang %d: '%s'\n",i,strlen(line),line);
 
-		int sent = sendFrame(sockfd, line, strlen(line));
 
-		printf("Kiriman selesai\n");
+int main(int argc, char *argv[]) {
 
-		i++;
+    /* code */
 
-		usleep(25 * 1000);
+	// Global variables through multiple processes
+	recieved = mmap(NULL, sizeof *recieved, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	*recieved = XON;
+
+
+	shtdown = mmap(NULL, sizeof *shtdown, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	*shtdown = false;
+
+	// Argument validation
+    if (argc < 4) {
+       fprintf(stderr,"usage %s hostname port filename\n", argv[0]);
+       exit(0);
     }
 
-	fclose(f);
+    // Connection initialization
+    int sockfd = initConnecton(argc, argv);
 
-	printf("Mengakhiri koneksi\n");
-	close(sockfd);
-	
-	
+    // Beaucrachy for sending and receiving thorugh UDP
+    struct sockaddr_storage peer_addr;
+    socklen_t peer_addr_len;
+
+
+    //Forking processes
+	pid_t  pid;
+	pid = fork();
+
+	if (pid == 0)  {
+		
+		// Child process for receiving ack
+
+		ACKFormat recv = recvACK(sockfd,peer_addr,peer_addr_len);
+
+		if (testChecksumACK(recv)) {
+			if (recv.ack == ACK) {
+				printf("Pesan terkirim\n");
+			} else {
+				printf("Pesan tidak sampai\n");
+			}
+		} else {
+			printf("ACK corrupted\n");
+		}
+
+		printf("Child finished\n");
+
+	} else {
+
+		// Parent process for sending message
+
+		// Binding to a file
+	    char filename[256];
+	    strcpy(filename,argv[3]);
+		FILE *f = fopen(filename,"rb");
+
+    	char red;
+    	int i = 0;
+    	Byte message[MessageLength];
+
+    	// Framing message per MessageLength and sending it
+		while (fscanf(f, "%c", &red) != EOF) {
+			if (i < MessageLength) {
+				message[i] = red;
+			} else {
+				sendFrame(sockfd,createFrame(1,message),peer_addr,peer_addr_len);
+				i = 0;
+			}
+			i++;
+		}
+
+		// Sending message shorter than MessageLength
+		if (i != 0) {
+			for (int j=i;j<MessageLength;j++) {
+				message[j] = 0;
+			}
+			sendFrame(sockfd,createFrame(1,message),peer_addr,peer_addr_len);
+		}
+
+		fclose(f);
+		*shtdown = true;
+		
+		wait(NULL);
+
+		// Freeing global variables' memory
+		munmap(recieved, sizeof *recieved);
+		munmap(shtdown, sizeof *shtdown);
+
+
+		// Terminating connection
+		printf("Mengakhiri koneksi\n");
+		close(sockfd);
+
+	}
+
 	return 0;
 }
 	
